63bf3c4b5c670e542af116bca2586a4c7cb23099
James Sadler <jamesironmanjames@gmail.com>
2024-02-03 02:04:55 -0600

    Update javascript code

--- static/index.js
+++ static/index.js
@@ -1,57 +1,26 @@
-//5ww2q
-let moneyElement= document.getElementById("user-score");
-let gameButton= document.querySelector(".CSS-clickgamebutton")
-let farmBuy= document.querySelector(".CSS-buybutton")
-let farmCount= document.querySelector(".JS-farmcount")
-let farmPriceEle= document.querySelector(".CSS-itemcost");
-let farmPrice = 10;
+const moneyElement= document.getElementById("user-score");
+const gameButton= document.querySelector(".CSS-clickgamebutton")
+const farmBuy= document.querySelector(".CSS-buybutton")
+const farmCount= document.querySelector(".JS-farmcount")
+const farmPriceEle= document.querySelector(".CSS-itemcost");
+const farmPrice = 10;
+let clicks = 0;
+let secondProfit = 0;
 
+const clicked = () => clicks += 1;
 
-let gameData = {
-    "clicks": 0,
-    "farmPrice":10,
-}
+const addMoney = () => {
+    moneyElement.innerHTML = Number(moneyElement.innerHTML) + clicks
+    clicks = 0;    
+};
 
 if (!moneyElement.innerHTML)
     moneyElement.innerHTML= Number(0);
+if (!farmCount.innerHTML)
+    farmCount.innerHTML= Number(0);
 
 
-gameButton.addEventListener("click",() => 
-gameData["clicks"] += 1
-);
+gameButton.addEventListener("click",clicked)
 
-farmBuy.addEventListener("click",buyFarm)
+setInterval(addMoney,500)
 
-setInterval(updateMoney, 1000)
-
-
-function updateMoney(){
-    let toAdd = 0;
-    toAdd += gameData["clicks"];
-    //  gameData["clicks"] = 0;
-
-    let money = Number(moneyElement.innerText)
-
-    moneyElement.innerText = money + toAdd;
-}
-
-
-
-/*
-
-function incrementScore(){
-    moneyElement.innerHTML = Number(moneyElement.innerHTML) + 1
-    currentMoney= moneyElement.innerHTML
-}
-
-function buyFarm(){
-    if (moneyElement.innerHTML - farmPrice > 0){
-        farmPrice = Math.round(farmPrice * 1.1);
-        farmPriceEle.innerHTML= farmPrice;
-        moneyElement.innerHTML= Number(moneyElement.innerHTML) - farmPrice;
-        farmCount.innerHTML = Number
-        (farmCount.innerHTML) + 1;
-        moneyRate += 1;
-    }
-}
-*/
