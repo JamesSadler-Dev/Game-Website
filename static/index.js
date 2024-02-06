03f54a159c2e93a4d659ac3dec4797da824b7a65
James Sadler <jamesironmanjames@gmail.com>
2024-02-05 22:59:58 -0600

    Add farm picture to game

--- static/index.js
+++ static/index.js
@@ -8,16 +8,13 @@ let clicks = 0;
 let secondProfit = 0;
 
 const clicked = () => clicks += 1;
-
 const addProfit = (buildingProfit) => {
     secondProfit += buildingProfit;
 }
-
 const incrementMoney = () => {
     moneyElement.innerHTML = Number(moneyElement.innerHTML) + clicks + secondProfit;
     clicks=0
 }
-
 const buyFarm = () => {
     if (Number(moneyElement.innerHTML) - farmPrice >= 0){
         moneyElement.innerHTML = moneyElement.innerHTML - farmPrice;
@@ -28,14 +25,12 @@ const buyFarm = () => {
     }
 }
 
-
 if (!moneyElement.innerHTML)
     moneyElement.innerHTML= Number(0);
 if (!farmCount.innerHTML)
     farmCount.innerHTML= Number(0);
 
-
 gameButton.addEventListener("click",clicked)
 farmBuy.addEventListener("click",buyFarm)
 setInterval(incrementMoney,500)
-
+localStorage.setItem()
